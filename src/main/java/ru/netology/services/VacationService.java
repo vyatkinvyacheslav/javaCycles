c53f293ef739b367bb06d55368a22898775175c4
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2023-06-30 23:50:32 +0500

    second commit

--- src/main/java/ru/netology/services/VacationService.java
+++ src/main/java/ru/netology/services/VacationService.java
@@ -4,6 +4,7 @@ public class VacationService {
     public int calculate(int income, int expenses, int threshold) {
         int counter = 0;
         int money = 0;
+
         for (int i = 0; i < 12; i++) {
             if (money >= threshold) {
                 money -= expenses;
--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -3,22 +3,25 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 class VacationServiceTest {
-    @Test
+  @Test
     public void testCalculate1() {
-        VacationService service = new VacationService();
-        int actual = service.calculate(income:10000, expenses:3000, threshold:20000);
-        int expected = 3;
-        Assertions.assertEquals(expected, actual);
+      VacationService service = new VacationService();
 
-    }
+      int actual = service.calculate(10000, 3000, 20000);
+      int expected = 3;
 
-    @Test
-    public void testCalculate2() {
-        VacationService service = new VacationService();
-        int actual = service.calculate(income:100_000, expenses:60_000, threshold:150_000);
-        int expected = 2;
-        Assertions.assertEquals(expected, actual);
+      Assertions.assertEquals(expected, actual);
+  }
 
-    }
+  @Test
+  public void testCalculate2() {
+    VacationService service = new VacationService();
+
+    int actual = service.calculate(100000, 60000, 150000);
+    int expected = 2;
+
+    Assertions.assertEquals(expected, actual);
+  }
 }
